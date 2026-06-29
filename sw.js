0c6e5998c26644eb6632e71d55d7811cf20243f4
Uriel Ricaurte <uriel.ricaurte.pm@gmail.com>
2026-06-28 22:14:43 -0400

    fix: bump service worker cache version to force refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tempo-v1';
+const CACHE = 'tempo-v26';
 const ASSETS = ['.', 'index.html', 'manifest.webmanifest', 'icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
