7d8284d2538128b54f44df537b3cbe22bafdd92d
Uriel Ricaurte <uriel.ricaurte.pm@gmail.com>
2026-07-10 02:13:12 -0400

    feat(ui): add native share/save sheet for PDF exports on mobile devices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tempo-v130';
+const CACHE = 'tempo-v131';
 const ASSETS = ['.', 'index.html', 'manifest.webmanifest', 'icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
