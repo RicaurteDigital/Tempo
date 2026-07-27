f6070cb41e185100eec7b486dec3b9c8c482ee11
Uriel Ricaurte <uriel.ricaurte.pm@gmail.com>
2026-07-27 16:11:56 -0400

    style(ui): add light mode global CSS variables to index.html

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tempo-v222';
+const CACHE = 'tempo-v223';
 const ASSETS = ['.', 'index.html', 'manifest.webmanifest', 'icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
