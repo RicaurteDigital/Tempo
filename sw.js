32e4c6fd9069166e62e2653f6f0e0994d85802ee
Uriel Ricaurte <uriel.ricaurte.pm@gmail.com>
2026-07-28 15:15:13 -0400

    chore(html): add cache buster versions to asset tags

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tempo-v256';
+const CACHE = 'tempo-v257';
 const ASSETS = ['.', 'index.html', 'manifest.webmanifest', 'icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
